0aa449a2f050eb056569bb47f902355e644fcc4b
wnsrl <wnsrl981228@gmail.com>
2024-02-28 22:05:05 +0900

    first commit

---
+++ src/main/java/com/editornice/EditorniceApplication.java
@@ -0,0 +1,13 @@
+package com.editornice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorniceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorniceApplication.class, args);
+    }
+
+}
