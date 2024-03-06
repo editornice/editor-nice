bcbed12c46bc501a279e0988f7b00df53b78076b
wnsrl <wnsrl981228@gmail.com>
2024-03-07 01:02:23 +0900

    test : 각 테스트 시작전 db 초기화 작업

--- src/test/java/com/editornice/company/service/CompanyServiceTest.java
+++ src/test/java/com/editornice/company/service/CompanyServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Sql({"/h2-truncate.sql"})
 class CompanyServiceTest {
 
     @Autowired
